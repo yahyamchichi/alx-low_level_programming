0eb5a46f1121f9d9e827ccb20e80cf871655e6af
yahyamchichi <0695383501yahya@gmail.com>
2023-03-16 23:19:42 +0000

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+#include <stdio.h)
+/**
+*main - main entry point
+*C HELLO WORL
+*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
