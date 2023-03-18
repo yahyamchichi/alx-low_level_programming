3e0951759f3a4e3ff9ea5118254ddffe038e5f22
yahyamchichi <0695383501yahya@gmail.com>
2023-03-18 00:45:53 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
