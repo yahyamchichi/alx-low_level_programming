943baa543f165fbc2d741b1930dc8a56cc3141e1
yahyamchichi <0695383501yahya@gmail.com>
2023-03-18 00:19:54 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int l = 97;
+	int u = 65;
+
+	while (l <= 122)
+	{
+		putchar(l);
+		l++;
+	}
+	while (u <= 90)
+	{
+		putchar(u);
+		u++;
+	}
+	putchar('\n');
+	return (0) ;
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
