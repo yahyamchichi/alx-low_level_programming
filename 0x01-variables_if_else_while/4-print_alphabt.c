baa1d91bf8fef50dd4a251b7bf2d070f5bdb09d8
yahyamchichi <0695383501yahya@gmail.com>
2023-03-18 00:40:13 +0000

    Print all letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,4 +7,18 @@
  */
 int main(void)
 {
+	int n = 97;
 
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
