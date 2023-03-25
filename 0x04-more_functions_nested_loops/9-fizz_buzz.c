76ad1b822b1d5e863b656bb5e0a0c0db7356183d
yahyamchichi <0695383501yahya@gmail.com>
2023-03-25 02:28:16 +0000

    Triangles

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,52 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			_putchar("F");
+			_putchar("i");
+			_putchar("z");
+			_putchar("z");
+			_putchar("B");
+			_putchar("u");
+			_putchar("z");
+			_putchar("z");
+			_putchar(" ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			_putchar("F");
+			_putchar("i");
+			_putchar("z");
+			_putchar("z");
+			_putchar(" ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			_putchar("B");
+			_putchar("u");
+			_putchar("z");
+			_putchar("z");
+			_putchar(" ");
 		}
 		else
 		{
-			printf("%d", i);
+			int n = i;
+
+			do
+			{
+				_putchar(n % 10 + '0');
+
+				n /= 10;
+			}
+			while (n > 0)
+
+			_putchar(' ');
 		}
 	}
-	printf("\n");
-
+	_putchar('\n');
+	return (0);
+}
+		}
+	}
+	_putchar("\n");
 	return (0);
 }
