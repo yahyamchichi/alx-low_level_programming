b13823849f71f559360d527822c0fd2dbf306ff3
yahyamchichi <0695383501yahya@gmail.com>
2023-04-03 23:24:54 +0000

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,24 +5,18 @@
  * @accept: input
  * Return: Always 0 (Success)
  */
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int r;
+	int k;
 
 	while (*s)
 	{
-		for (r = 0; accept[r]; r++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-			return (n);
+			if (*s == accept[k])
+				return (s);
 		}
-		s++;
+	s++;
 	}
-	return (n);
+	return ('\0');
 }
