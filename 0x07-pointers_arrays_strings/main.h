b711782dd7b0b22c042cd0643680ad208769dec5
yahyamchichi <0695383501yahya@gmail.com>
2023-04-03 22:46:54 +0000

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
