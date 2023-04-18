e4413617c198072f9ea79bb2247a51bd78b13d67
yahyamchichi <0695383501yahya@gmail.com>
2023-04-18 00:38:54 +0000

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: This structure represents
+ * a dog and its associated information.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
